ad6d344b8af5441f6d910f58dd58d0f92f940832
Michael Lunzer <michaellunzer@users.noreply.github.com>
2023-03-18 23:19:56 -0700

    adding Spotify Monthly Plalists 2014-2023

---
+++ src/components/spotifyMonthlyPlaylists2021.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image";
+
+export const SpotifyMonthlyPlaylists2021 = () => {
+    const data = useStaticQuery(graphql`query SpotifyMonthlyPlaylists2021 {
+  allSpotifyPlaylist(
+    filter: {name: {glob: "*2021*"}}
+    sort: {fields: order}
+    limit: 1500
+  ) {
+    edges {
+      node {
+        name
+        description
+        tracks {
+          total
+        }
+        external_urls {
+          spotify
+        }
+        href
+        uri
+        order
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(width: 800, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`)
+    return <>
+        { data.allSpotifyPlaylist.edges &&
+            <div className="spotify__top-artists">
+                <h1>2021</h1>
+                <p>
+                    {/* I make a monthly playlist to record the songs that I'm currently listening to. Kinda like a musical time capsule!
+                    I've been doing this on Spotify off and on since 2014 (and before that using iTunes 😎). This list only shows the last ~50 months, so check out my Spotify profile to go further back. */}
+                </p>
+                <div className="spotify__list">
+                    {data.allSpotifyPlaylist.edges.map((name, index) =>
+                        <div key={index} className="spotify__list-item d-flex">
+                            <div className="spotify__album-cover">
+                                <GatsbyImage image={name.node.image.localFile.childImageSharp.gatsbyImageData} />
+                            </div>
+                            <h3>
+                                <span className="spotify__list-item-number">{index + 1}</span>
+                                <a href={ name.node.external_urls.spotify}>{ name.node.name }</a>
+                            </h3>
+                        </div>
+                    )}
+                </div>
+            </div>
+      }
+    </>;
+}
+
+export default SpotifyMonthlyPlaylists2021
