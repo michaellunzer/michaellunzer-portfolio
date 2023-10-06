ce8115e5dc68d6f4bebf91392b319e1d11432fa5
Michael Lunzer <michaellunzer@users.noreply.github.com>
2023-10-05 18:06:59 -0700

    cleaning up spotify 2019 and adding spotify 2024

--- src/pages/spotify.js
+++ src/pages/spotify.js
@@ -2,11 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SpotifyTopArtists from "../components/spotifyTopArtists"
+// import SpotifyMonthlyPlaylists2024 from "../components/spotifyMonthlyPlaylists2024"
 import SpotifyMonthlyPlaylists2023 from "../components/spotifyMonthlyPlaylists2023"
 import SpotifyMonthlyPlaylists2022 from "../components/spotifyMonthlyPlaylists2022"
 import SpotifyMonthlyPlaylists2021 from "../components/spotifyMonthlyPlaylists2021"
 import SpotifyMonthlyPlaylists2020 from "../components/spotifyMonthlyPlaylists2020"
-import SpotifyMonthlyPlaylists2019 from "../components/spotifyMonthlyPlaylists2019"
+// import SpotifyMonthlyPlaylists2019 from "../components/spotifyMonthlyPlaylists2019"
 // import SpotifyMonthlyPlaylists2018 from "../components/spotifyMonthlyPlaylists2018"
 // import SpotifyMonthlyPlaylists2017 from "../components/spotifyMonthlyPlaylists2017"
 // import SpotifyMonthlyPlaylists2016 from "../components/spotifyMonthlyPlaylists2016"
@@ -31,12 +32,13 @@ const Spotify = () => (
             {/* <div className="resume-flex"> */}
             <SpotifyTopArtists />
 
+            {/* <SpotifyMonthlyPlaylists2024 /> */}
             <SpotifyMonthlyPlaylists2023 />
             <SpotifyMonthlyPlaylists2022 />
             <SpotifyMonthlyPlaylists2021 />
             <SpotifyMonthlyPlaylists2020 />
-            <SpotifyMonthlyPlaylists2019 />
-            {/* <SpotifyMonthlyPlaylists2018 />
+            {/* <SpotifyMonthlyPlaylists2019 />
+            <SpotifyMonthlyPlaylists2018 />
             <SpotifyMonthlyPlaylists2017 />
             <SpotifyMonthlyPlaylists2016 />
             <SpotifyMonthlyPlaylists2015 />
