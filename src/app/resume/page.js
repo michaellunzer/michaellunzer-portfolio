1590b09bcb5a63f2833f1e5cf3a7add46e500c35
Michael Lunzer <michaellunzer@users.noreply.github.com>
2025-07-31 21:17:52 -0700

    commenting out spotify page and resume

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,27 +1,27 @@
-import { getSiteInformation } from '../../../lib/contentful'
-import Layout from '../../components/layout'
-import SEO from '../../components/seo'
-import ResumeDownload from '../../components/ResumeDownload'
+// import { getSiteInformation } from '../../../lib/contentful'
+// import Layout from '../../components/layout'
+// import SEO from '../../components/seo'
+// import ResumeDownload from '../../components/ResumeDownload'
 
-export default async function ResumePage() {
-  const siteInfo = await getSiteInformation()
+// export default async function ResumePage() {
+//   const siteInfo = await getSiteInformation()
 
-  return (
-    <Layout header="resume" siteInfo={siteInfo}>
-      <SEO
-        title="Resume"
-        keywords={['Michael Lunzer', 'Resume', 'CV', 'Experience']}
-        siteInfo={siteInfo}
-      />
-      <div className="site-container">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <ResumeDownload />
-            </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
-} 
+//   return (
+//     <Layout header="resume" siteInfo={siteInfo}>
+//       <SEO
+//         title="Resume"
+//         keywords={['Michael Lunzer', 'Resume', 'CV', 'Experience']}
+//         siteInfo={siteInfo}
+//       />
+//       <div className="site-container">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-sm-12">
+//               <ResumeDownload />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </Layout>
+//   )
+// } 
